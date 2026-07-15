d2cb6077aecdccd15b58066a4db869f24973726c
thanhbui1234 <miktenthanh@gmail.com>
2026-07-16 02:41:08 +0700

    fix

--- apps/user-next-app/src/lib/api.ts
+++ apps/user-next-app/src/lib/api.ts
@@ -1,4 +1,5 @@
-import { ClientApi, createFetchClient } from 'shared-api';
+import { createFetchClient } from './fetch-client';
+import { createApiClient } from '@/api/client';
 
 // 1. Khởi tạo lõi Fetch (Cấu hình domain, token...)
 export const fetchClient = createFetchClient({
@@ -13,5 +14,5 @@ export const fetchClient = createFetchClient({
 });
 
 // 2. Khởi tạo MỘT object SDK duy nhất bao gồm tất cả API
-export const api = ClientApi.createApiClient(fetchClient);
+export const api = createApiClient(fetchClient);
 
