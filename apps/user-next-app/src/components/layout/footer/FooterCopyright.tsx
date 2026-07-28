0e12e7d262637e10d31ce27a1618527d9408b8c2
thanhbui1234 <miktenthanh@gmail.com>
2026-07-28 21:59:18 +0700

    update footer

--- apps/user-next-app/src/components/layout/footer/FooterCopyright.tsx
+++ apps/user-next-app/src/components/layout/footer/FooterCopyright.tsx
@@ -2,15 +2,8 @@ import Link from 'next/link';
 
 export const FooterCopyright = () => {
   return (
-    <div className="relative z-[2] flex flex-wrap items-center justify-between gap-2 py-5 px-10 text-[12px] text-white">
-      <div className="flex flex-wrap gap-4">
-        <Link href="/cookie-declaration/" className="text-white no-underline text-[12px]">Cookie Declaration</Link>
-        <span className="text-white/50">|</span>
-        <span className="text-white">Cẩn thận hàng giả!</span>
-        <span className="text-white/50">|</span>
-        <Link href="/about-website/" className="text-white no-underline text-[12px]">Về trang web này</Link>
-      </div>
-      <a href="https://www.xuantruongstack.com/" target="_blank" rel="noopener noreferrer">Copyright (C) 2626 https://www.xuantruongstack.com</a>
+    <div className="relative  flex flex-wrap items-center justify-center gap-2 py-5 px-10 text-[12px] text-white">
+      <a className='text-white no-underline text-[12px] cursor-pointer' href="https://www.xuantruongstack.com/" target="_blank" rel="noopener noreferrer">Copyright (C) 2626 xuantruongstack.com</a>
     </div>
   );
 };
